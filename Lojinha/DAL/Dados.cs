643e96c0356887b456ab51b243c223b04406b179
65caritina65 <65caritina65@gmail.com>
2026-05-11 17:50:24 -0400

    StringConexao

---
+++ Lojinha/DAL/Dados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.DAL
+{
+    public class Dados
+    {
+        public static string StringConexao
+        {
+            get
+            {
+                return "server = Data Source=PCAURORA;Initial Catalog=dblojinha;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+            }
+        }
+    }
+}
+
+
+
+
